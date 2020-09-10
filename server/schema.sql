b1893bd903042774d808882c602269b307a2c177
Luke Eastman <LukeAEastman@gmail.com>
2020-09-09 17:01:09 -0700

    build initial mysql db

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,54 @@
+DROP DATABASE chat;
 CREATE DATABASE chat;
-
 USE chat;
-
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id INT AUTO_INCREMENT,
+  user_id INT,
+  room_id INT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  message_text TEXT,
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT,
+  name TEXT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms_messsages (
+  id INT AUTO_INCREMENT,
+  room_id INT,
+  message_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT,
+  name TEXT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users_friends (
+  id INT AUTO_INCREMENT,
+  user_id INT,
+  friend_user_id INT,
+  PRIMARY KEY (id)
+);
+
+ALTER TABLE messages
+ADD FOREIGN KEY (user_id) REFERENCES users(id),
+ADD FOREIGN KEY (room_id) REFERENCES rooms(id);
+
+ALTER TABLE rooms_messsages
+ADD FOREIGN KEY (room_id) REFERENCES rooms(id),
+ADD FOREIGN KEY (message_id) REFERENCES messages(id);
+
+ALTER TABLE users_friends
+ADD FOREIGN KEY (user_id) REFERENCES users(id),
+ADD FOREIGN KEY (friend_user_id) REFERENCES users(id);
+-- <NAME-OF-COL><DATA-TYPE-OF-COL>[addition info],
+
 
 /* Create other tables and define schemas for them here! */
 
